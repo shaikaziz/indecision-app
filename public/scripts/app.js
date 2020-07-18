771e3a9647397c1ac4f272458c28bd365a647479
Shaik Aziz <shaikazizz98@gmail.com>
2020-07-18 12:23:23 +0530

    updates in components

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -8,8 +8,39 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Header = function (_React$Component) {
-    _inherits(Header, _React$Component);
+var Randomizer = function (_React$Component) {
+    _inherits(Randomizer, _React$Component);
+
+    function Randomizer() {
+        _classCallCheck(this, Randomizer);
+
+        return _possibleConstructorReturn(this, (Randomizer.__proto__ || Object.getPrototypeOf(Randomizer)).apply(this, arguments));
+    }
+
+    _createClass(Randomizer, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                null,
+                React.createElement(
+                    "h1",
+                    null,
+                    "Randomizer"
+                ),
+                React.createElement(Header, null),
+                React.createElement(Action, null),
+                React.createElement(Options, null),
+                React.createElement(AddOption, null)
+            );
+        }
+    }]);
+
+    return Randomizer;
+}(React.Component);
+
+var Header = function (_React$Component2) {
+    _inherits(Header, _React$Component2);
 
     function Header() {
         _classCallCheck(this, Header);
@@ -18,12 +49,12 @@ var Header = function (_React$Component) {
     }
 
     _createClass(Header, [{
-        key: 'render',
+        key: "render",
         value: function render() {
             return React.createElement(
-                'p',
+                "p",
                 null,
-                'Header'
+                "Let me make a random decision for you"
             );
         }
     }]);
@@ -31,15 +62,91 @@ var Header = function (_React$Component) {
     return Header;
 }(React.Component);
 
-var jsx = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        'this is title'
-    ),
-    React.createElement(Header, null)
-);
+var Action = function (_React$Component3) {
+    _inherits(Action, _React$Component3);
 
-ReactDOM.render(jsx, document.getElementById('app'));
+    function Action() {
+        _classCallCheck(this, Action);
+
+        return _possibleConstructorReturn(this, (Action.__proto__ || Object.getPrototypeOf(Action)).apply(this, arguments));
+    }
+
+    _createClass(Action, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                null,
+                React.createElement(
+                    "button",
+                    null,
+                    "Select Random"
+                )
+            );
+        }
+    }]);
+
+    return Action;
+}(React.Component);
+
+var AddOption = function (_React$Component4) {
+    _inherits(AddOption, _React$Component4);
+
+    function AddOption() {
+        _classCallCheck(this, AddOption);
+
+        return _possibleConstructorReturn(this, (AddOption.__proto__ || Object.getPrototypeOf(AddOption)).apply(this, arguments));
+    }
+
+    _createClass(AddOption, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                null,
+                React.createElement(
+                    "form",
+                    null,
+                    React.createElement("input", { type: "text", placeholder: "Enter Your Option" }),
+                    "\xA0",
+                    React.createElement(
+                        "button",
+                        null,
+                        "Add Option"
+                    )
+                )
+            );
+        }
+    }]);
+
+    return AddOption;
+}(React.Component);
+
+var Options = function (_React$Component5) {
+    _inherits(Options, _React$Component5);
+
+    function Options() {
+        _classCallCheck(this, Options);
+
+        return _possibleConstructorReturn(this, (Options.__proto__ || Object.getPrototypeOf(Options)).apply(this, arguments));
+    }
+
+    _createClass(Options, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                null,
+                React.createElement(
+                    "h3",
+                    null,
+                    "These are your options"
+                )
+            );
+        }
+    }]);
+
+    return Options;
+}(React.Component);
+
+ReactDOM.render(React.createElement(Randomizer, null), document.getElementById('app'));
--- src/app.js
+++ src/app.js
@@ -1,14 +1,56 @@
-class Header extends React.Component {
-    render(){
-        return <p>Header</p>
+class Randomizer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Randomizer</h1>
+                <Header />
+                <Action />
+                <Options/>
+                <AddOption/>
+            </div>
+        );
     }
 }
 
-const jsx = (
-    <div>
-        <h1>this is title</h1>
-        <Header />
-    </div>
-);
+class Header extends React.Component {
+    render(){
+        return <p>Let me make a random decision for you</p>
+    }
+}
 
-ReactDOM.render(jsx, document.getElementById('app'));
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>Select Random</button>
+            </div>
+        );
+    }
+}
+
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text" placeholder="Enter Your Option"></input>
+                    &nbsp;<button>Add Option</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>These are your options</h3>
+            </div>
+        );
+    }
+}
+
+
+
+ReactDOM.render(<Randomizer/>, document.getElementById('app'));
